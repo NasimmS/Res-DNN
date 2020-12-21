eedaf879f6a637380dbbdf90088df139c886a3a6
NasimmS <samimi.nassim@gmail.com>
2020-12-21 15:50:48 +0330

    Add files via upload

---
+++ Hardware Design/Source/binary_comparator.v
@@ -0,0 +1,30 @@
+////////////////////////////////////////////////////////////////////////////////
+module bin_comp(a,b,aeqb,agtb,altb);
+input [3:0] a,b;
+output aeqb,agtb,altb;
+reg aeqb,agtb,altb;
+
+always @(a or b)
+begin
+aeqb=0; agtb=0; altb=0;
+if(a==b)
+aeqb=1;
+else if (a>b)
+
+agtb=1;
+else
+altb=1;
+end
+endmodule
+
+
+module b_comp1 (a, b, L, E,G); 
+input a, b; output L, E, G;
+wire s1, s2;
+not X1(s1, a);
+not X2 (s2, b);
+and X3 (L,s1, b);
+and X4 (G,s2, a);
+xnor X5 (E, a, b);
+endmodule
+
---
+++ Hardware Design/Source/regfile.v
@@ -0,0 +1,20 @@
+/////////////////////////////////////////////////////////////////////////////////
+module regfile(input clk, input [3:0] address, input en_write, inout [7:0] data);
+
+// Register file storage
+reg [7:0] registers[11:0];
+reg [7:0] out_val;
+// Read and write from register file
+always @(posedge clk)
+    if (en_write)
+        registers[address] <= data;
+    else
+        out_val <= registers[address];
+
+// Output data if not writing. If we are writing,
+// do not drive output pins. This is denoted
+// by assigning 'z' to the output pins.
+
+assign data = en_write ? 8'bz : out_val;
+
+endmodule
